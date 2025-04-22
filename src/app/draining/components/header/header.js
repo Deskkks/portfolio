a36b961980b22b7943a092d6db93ddc70d377a48
Kayky <kaykyalmeida@hotmail.com.br>
2025-04-22 11:44:15 -0300

    vercel test2

--- src/app/draining/components/header/header.js
+++ src/app/draining/components/header/header.js
@@ -1,13 +1,9 @@
 ﻿import styles from "./header.module.css"
 import Button from "../button/button";
 import Link from "next/link";
-import { cookies } from "next/headers";
 import Image from "next/image";
 
 export default async function Header() {
-
-  const cookieStore = await cookies()
-  const login = cookieStore.get('login')
 
   return(
     <header className={styles.header}>
@@ -26,20 +22,9 @@ export default async function Header() {
         </div>
       </Link>
       <div className={styles.conteiner}>
-        { login ? (
-          <>
-            <Link className={styles.button} href={"/draining/dashboard"}>
-              <Button text={"DashBoard"}/>
-            </Link>
-            <Link href={"/api/user/logout"} className={styles.button}>
-              <Button text={"Sair"}/>
-            </Link>
-          </>
-        ) : (
-          <Link href={"draining/login"} className={styles.button}>
-            <Button text={"Entrar"}/>
-          </Link>
-        )}
+        <Link className={styles.button} href={"/draining/dashboard"}>
+          <Button text={"DashBoard"}/>
+        </Link>
         <Link href={"/draining/sobre"} className={styles.button}>
           <Button text={"Sobre"}/>
         </Link>
